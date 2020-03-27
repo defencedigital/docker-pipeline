aabc9937af5b98d3ff4f666b3b3ef72905f9ba24
Digitiain <iain@digital.mod.uk>
2020-03-27 18:10:43 +0000

    Load test data when running dev Dockerfile

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -14,6 +14,7 @@ fi
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
+python manage.py loaddata test-data.json
 python manage.py collectstatic --no-input --clear
 
 exec "$@"
